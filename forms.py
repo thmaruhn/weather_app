84dfac4f6392a301cd26a3f01f83a50d2ec747ed
thmaruhn <t.maruhn@mailbox.org>
2023-07-11 17:20:30 +0200

    better handling file input form

--- forms.py
+++ forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, FileField
+from wtforms import StringField, DateField
 from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileRequired
+
 from datetime import date
 
 class SearchForm(FlaskForm):
@@ -8,5 +10,5 @@ class SearchForm(FlaskForm):
     START_DATE = DateField('From date', default=date.today(), validators=[InputRequired()],)
     END_DATE = DateField('To date', default=date.today(), validators=[InputRequired()])
     API_KEY = StringField('API Key', validators=[InputRequired()], description='Provide valid API key')
-    GOOGLE_APPLICATION_CREDENTIALS_PATH = FileField('Google Application Credentials Path', validators=[InputRequired()])
+    GOOGLE_APPLICATION_CREDENTIALS_PATH = FileField('Google Application Credentials Path', validators=[FileRequired()])
     BIG_QUERY_TARGET_ID = StringField('BigQuery Target ID', validators=[InputRequired()], description='Provide BigQuery Target ID')
